30787303e266ebda9bbb688bc4eafa85f84cf974
Vicky Kumar <147160400+Vickykumar2023@users.noreply.github.com>
2024-02-28 20:04:15 +0530

    Add files via upload

---
+++ dilog.js
@@ -0,0 +1,359 @@
+const dilog1=document.querySelector('#dilog1');
+const opendilog1=document.querySelector('.open-button');
+const closedilog1=document.querySelector('.close-button');
+
+opendilog1.addEventListener('click', () =>{
+    dilog1.show();
+})
+
+closedilog1.addEventListener('click', () =>{
+    dilog1.close();
+})
+// =============================================================================================
+const dilog2=document.querySelector('#dilog2');
+const opendilog2=document.querySelector('.open2');
+const closedilog2=document.querySelector('.close2');
+
+opendilog2.addEventListener('click', () =>{
+    dilog2.show();
+})
+
+closedilog2.addEventListener('click', () =>{
+    dilog2.close();
+})
+// =====================================================================================================
+const dilog3=document.querySelector('#dilog3');
+const opendilog3=document.querySelector('.open3');
+const closedilog3=document.querySelector('.close3');
+
+opendilog3.addEventListener('click', () =>{
+    dilog3.show();
+})
+
+closedilog3.addEventListener('click', () =>{
+    dilog3.close();
+})
+// =====================================================================================================
+const dilog4=document.querySelector('#dilog4');
+const opendilog4=document.querySelector('.open4');
+const closedilog4=document.querySelector('.close4');
+
+opendilog4.addEventListener('click', () =>{
+    dilog4.show();
+})
+
+closedilog4.addEventListener('click', () =>{
+    dilog4.close();
+})
+// =====================================================================================================
+const dilog5=document.querySelector('#dilog5');
+const opendilog5=document.querySelector('.open5');
+const closedilog5=document.querySelector('.close5');
+
+opendilog5.addEventListener('click', () =>{
+    dilog5.show();
+})
+
+closedilog5.addEventListener('click', () =>{
+    dilog5.close();
+})
+// =====================================================================================================
+const dilog6=document.querySelector('#dilog6');
+const opendilog6=document.querySelector('.open6');
+const closedilog6=document.querySelector('.close6');
+
+opendilog6.addEventListener('click', () =>{
+    dilog6.show();
+})
+
+closedilog6.addEventListener('click', () =>{
+    dilog6.close();
+})
+// =====================================================================================================
+const dilog7=document.querySelector('#dilog7');
+const opendilog7=document.querySelector('.open7');
+const closedilog7=document.querySelector('.close7');
+
+opendilog7.addEventListener('click', () =>{
+    dilog7.show();
+})
+
+closedilog7.addEventListener('click', () =>{
+    dilog7.close();
+})
+// =====================================================================================================
+const dilog8=document.querySelector('#dilog8');
+const opendilog8=document.querySelector('.open8');
+const closedilog8=document.querySelector('.close8');
+
+opendilog8.addEventListener('click', () =>{
+    dilog8.show();
+})
+
+closedilog8.addEventListener('click', () =>{
+    dilog8.close();
+})
+// =====================================================================================================
+const dilog9=document.querySelector('#dilog9');
+const opendilog9=document.querySelector('.open9');
+const closedilog9=document.querySelector('.close9');
+
+opendilog9.addEventListener('click', () =>{
+    dilog9.show();
+})
+
+closedilog9.addEventListener('click', () =>{
+    dilog9.close();
+})
+// =====================================================================================================
+const dilog10=document.querySelector('#dilog10');
+const opendilog10=document.querySelector('.open10');
+const closedilog10=document.querySelector('.close10');
+
+opendilog10.addEventListener('click', () =>{
+    dilog10.show();
+})
+
+closedilog10.addEventListener('click', () =>{
+    dilog10.close();
+})
+// =====================================================================================================
+const dilog11=document.querySelector('#dilog11');
+const opendilog11=document.querySelector('.open11');
+const closedilog11=document.querySelector('.close11');
+
+opendilog11.addEventListener('click', () =>{
+    dilog11.show();
+})
+
+closedilog11.addEventListener('click', () =>{
+    dilog11.close();
+})
+// =====================================================================================================
+const dilog12=document.querySelector('#dilog12');
+const opendilog12=document.querySelector('.open12');
+const closedilog12=document.querySelector('.close12');
+
+opendilog12.addEventListener('click', () =>{
+    dilog12.show();
+})
+
+closedilog12.addEventListener('click', () =>{
+    dilog12.close();
+})
+// =====================================================================================================
+const dilog13=document.querySelector('#dilog13');
+const opendilog13=document.querySelector('.open13');
+const closedilog13=document.querySelector('.close13');
+
+opendilog13.addEventListener('click', () =>{
+    dilog13.show();
+})
+
+closedilog13.addEventListener('click', () =>{
+    dilog13.close();
+})
+// =====================================================================================================
+const dilog14=document.querySelector('#dilog14');
+const opendilog14=document.querySelector('.open14');
+const closedilog14=document.querySelector('.close14');
+
+opendilog14.addEventListener('click', () =>{
+    dilog14.show();
+})
+
+closedilog14.addEventListener('click', () =>{
+    dilog14.close();
+})
+// =====================================================================================================
+const dilog15=document.querySelector('#dilog15');
+const opendilog15=document.querySelector('.open15');
+const closedilog15=document.querySelector('.close15');
+
+opendilog15.addEventListener('click', () =>{
+    dilog15.show();
+})
+
+closedilog15.addEventListener('click', () =>{
+    dilog15.close();
+})
+// =====================================================================================================
+const dilog16=document.querySelector('#dilog16');
+const opendilog16=document.querySelector('.open16');
+const closedilog16=document.querySelector('.close16');
+
+opendilog16.addEventListener('click', () =>{
+    dilog16.show();
+})
+
+closedilog16.addEventListener('click', () =>{
+    dilog16.close();
+})
+// =====================================================================================================
+const dilog17=document.querySelector('#dilog17');
+const opendilog17=document.querySelector('.open17');
+const closedilog17=document.querySelector('.close17');
+
+opendilog17.addEventListener('click', () =>{
+    dilog17.show();
+})
+
+closedilog17.addEventListener('click', () =>{
+    dilog17.close();
+})
+// =====================================================================================================
+const dilog18=document.querySelector('#dilog18');
+const opendilog18=document.querySelector('.open18');
+const closedilog18=document.querySelector('.close18');
+
+opendilog18.addEventListener('click', () =>{
+    dilog18.show();
+})
+
+closedilog18.addEventListener('click', () =>{
+    dilog18.close();
+})
+// =====================================================================================================
+const dilog19=document.querySelector('#dilog19');
+const opendilog19=document.querySelector('.open19');
+const closedilog19=document.querySelector('.close19');
+
+opendilog19.addEventListener('click', () =>{
+    dilog19.show();
+})
+
+closedilog19.addEventListener('click', () =>{
+    dilog19.close();
+})
+// =====================================================================================================
+const dilog20=document.querySelector('#dilog20');
+const opendilog20=document.querySelector('.open20');
+const closedilog20=document.querySelector('.close20');
+
+opendilog20.addEventListener('click', () =>{
+    dilog20.show();
+})
+
+closedilog20.addEventListener('click', () =>{
+    dilog20.close();
+})
+// =====================================================================================================
+const dilog21=document.querySelector('#dilog21');
+const opendilog21=document.querySelector('.open21');
+const closedilog21=document.querySelector('.close21');
+
+opendilog21.addEventListener('click', () =>{
+    dilog21.show();
+})
+
+closedilog21.addEventListener('click', () =>{
+    dilog21.close();
+})
+// =====================================================================================================
+const dilog22=document.querySelector('#dilog22');
+const opendilog22=document.querySelector('.open22');
+const closedilog22=document.querySelector('.close22');
+
+opendilog22.addEventListener('click', () =>{
+    dilog22.show();
+})
+
+closedilog22.addEventListener('click', () =>{
+    dilog22.close();
+})
+// =====================================================================================================
+const dilog23=document.querySelector('#dilog23');
+const opendilog23=document.querySelector('.open23');
+const closedilog23=document.querySelector('.close23');
+
+opendilog23.addEventListener('click', () =>{
+    dilog23.show();
+})
+
+closedilog23.addEventListener('click', () =>{
+    dilog23.close();
+})
+// =====================================================================================================
+const dilog24=document.querySelector('#dilog24');
+const opendilog24=document.querySelector('.open24');
+const closedilog24=document.querySelector('.close24');
+
+opendilog24.addEventListener('click', () =>{
+    dilog24.show();
+})
+
+closedilog24.addEventListener('click', () =>{
+    dilog24.close();
+})
+// =====================================================================================================
+const dilog25=document.querySelector('#dilog25');
+const opendilog25=document.querySelector('.open25');
+const closedilog25=document.querySelector('.close25');
+
+opendilog25.addEventListener('click', () =>{
+    dilog25.show();
+})
+
+closedilog25.addEventListener('click', () =>{
+    dilog25.close();
+})
+// =====================================================================================================
+const dilog26=document.querySelector('#dilog26');
+const opendilog26=document.querySelector('.open26');
+const closedilog26=document.querySelector('.close26');
+
+opendilog26.addEventListener('click', () =>{
+    dilog26.show();
+})
+
+closedilog26.addEventListener('click', () =>{
+    dilog26.close();
+})
+// =====================================================================================================
+const dilog27=document.querySelector('#dilog27');
+const opendilog27=document.querySelector('.open27');
+const closedilog27=document.querySelector('.close27');
+
+opendilog27.addEventListener('click', () =>{
+    dilog27.show();
+})
+
+closedilog27.addEventListener('click', () =>{
+    dilog27.close();
+})
+// =====================================================================================================
+const dilog28=document.querySelector('#dilog28');
+const opendilog28=document.querySelector('.open28');
+const closedilog28=document.querySelector('.close28');
+
+opendilog28.addEventListener('click', () =>{
+    dilog28.show();
+})
+
+closedilog28.addEventListener('click', () =>{
+    dilog28.close();
+})
+// =====================================================================================================
+const dilog29=document.querySelector('#dilog29');
+const opendilog29=document.querySelector('.open29');
+const closedilog29=document.querySelector('.close29');
+
+opendilog29.addEventListener('click', () =>{
+    dilog29.show();
+})
+
+closedilog29.addEventListener('click', () =>{
+    dilog29.close();
+})
+// =====================================================================================================
+const dilog30=document.querySelector('#dilog30');
+const opendilog30=document.querySelector('.open30');
+const closedilog30=document.querySelector('.close30');
+
+opendilog30.addEventListener('click', () =>{
+    dilog30.show();
+})
+
+closedilog30.addEventListener('click', () =>{
+    dilog30.close();
+})
